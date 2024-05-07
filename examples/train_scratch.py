d6b72565b390c8fa13753eca6db01616d3a7774a
Navya Jain <navya19j@gmail.com>
2024-05-07 17:18:32 -0400

    change td to dqn

--- examples/train_scratch.py
+++ examples/train_scratch.py
@@ -1,6 +1,7 @@
 # import gymnasium as gym
 # import gym
 from stable_baselines3 import TD3
+from stable_baselines3 import DQN
 from stable_baselines3.common.callbacks import EvalCallback
 import gymnasium as gym
 import sys
@@ -8,22 +9,29 @@ sys.modules["gym"] = gym
 
 def main():
     env = gym.make("ALE/MsPacman-v5", max_episode_steps=150)
-    model = TD3(
-        "MultiInputPolicy",
+    # any baseline model can be used here - Q learning
+    model = DQN(
+        "MlpPolicy",
         env,
         verbose=1,
-        tensorboard_log="logs/pointmaze_scratch"
     )
+    # model = TD3(
+    #     "MultiInputPolicy",
+    #     env,
+    #     verbose=1,
+    #     tensorboard_log="logs/pacman_scratch"
+    # )
     model.learn(
         total_timesteps=1e6,
-        log_interval=10,
         progress_bar=True,
         callback=EvalCallback(
             env,
             n_eval_episodes=100,
-            best_model_save_path="examples/models/pointmaze_scratch_TD3"
+            best_model_save_path="examples/models/pacman_scratch_DQN"
         ),
     )
+
+    model.save("examples/models/pacman_scratch_DQN")
 
 
 if __name__ == "__main__":
