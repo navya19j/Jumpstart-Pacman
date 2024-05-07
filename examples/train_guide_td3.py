7540e5e11811b42c0940de0510152236c1477181
Navya Jain <navya19j@gmail.com>
2024-05-07 16:58:15 -0400

    add_files

---
+++ examples/train_guide_td3.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+from stable_baselines3 import TD3
+from stable_baselines3.common.callbacks import EvalCallback
+
+
+def main():
+    env = gym.make("ALE/MsPacman-v5", max_episode_steps=150)
+    model = TD3(
+        "MultiInputPolicy",
+        env,
+        verbose=1,
+        tensorboard_log="logs/pacman_guide"
+    )
+    model.learn(
+        total_timesteps=1e5,
+        log_interval=10,
+        progress_bar=True,
+        callback=EvalCallback(
+            env,
+            n_eval_episodes=100,
+            best_model_save_path="examples/models/pacman_guide_TD3"
+        ),
+    )
+
+
+if __name__ == "__main__":
+    main()
