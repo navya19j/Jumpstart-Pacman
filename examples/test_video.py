bfa0010d3eadd88998fd198201236a3857c73ea9
Navya Jain <navya19j@gmail.com>
2024-05-09 01:30:13 -0400

    last

--- examples/test_video.py
+++ examples/test_video.py
@@ -127,7 +127,7 @@ model_2.policy.load_state_dict(policy)
 print("Saving the model")
 # SAVE_PATH
 # make directories if not exist
-os.makedirs(f"{BASE_PATH}/ppo_new", exist_ok=True)
-# also copy /home/nj2513/ppo/ALE-MsPacman-v5 folder to f"BASE_PATH}/ppo/
-shutil.copytree("/home/nj2513/ppo/ALE-MsPacman-v5", f"{BASE_PATH}/ppo_new/", dirs_exist_ok=True)
-model_2.save(f"{BASE_PATH}/ppo_new/ALE-MsPacman-v5")
+os.makedirs(f"{DIR_PATH}/ppo_new", exist_ok=True)
+# also copy /home/nj2513/ppo/ALE-MsPacman-v5 folder to f"DIR_PATH}/ppo/
+shutil.copytree("/home/nj2513/ppo/ALE-MsPacman-v5", f"{DIR_PATH}/ppo_new/", dirs_exist_ok=True)
+model_2.save(f"{DIR_PATH}/ppo_new/ALE-MsPacman-v5")
